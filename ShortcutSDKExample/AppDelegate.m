57ae4d5a9c6b7b2822ccde72419ade902c20da66
herbertbay <herbert.bay@gmail.com>
2018-03-28 11:07:31 +1300

    Change comment for testing

--- ShortcutSDKExample/AppDelegate.m
+++ ShortcutSDKExample/AppDelegate.m
@@ -2,7 +2,7 @@
 //  AppDelegate.m
 //  ShortcutSDKExample
 //
-//  Created by Severin Schoepke on 06/02/15.
+//  Created by Herbert Bay on 06/02/15.
 //  Copyright (c) 2015 Shortcut Media AG. All rights reserved.
 //
 
